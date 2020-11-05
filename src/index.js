dd204fc91ccd9edd1f27b7de7f8b9acbf2a0cfad
Ahmed Raza Qadri <razatechuc11@gmail.com>
2020-11-05 15:21:48 +0500

    Worked

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./components/App"
+import * as serviceWorker from "./service-worker";
+
 import "bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.render(
@@ -9,3 +11,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 )
+serviceWorker.register();
